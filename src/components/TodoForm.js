cf50ade760e85fb3ae29f91a3cad9edefa819bb7
edobbin <ethandobbin37@gmail.com>
2023-10-10 15:47:59 -0400

    init

---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e =>{
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random()*10000),
+            text: input
+        });
+        setInput('');
+    };
+  return (
+    <form class = "todo-form" onSubmit={handleSubmit}>
+        <input 
+            type='text' 
+            placeholder='add a todo' 
+            value={input} 
+            name = 'text' 
+            className='todo-input'
+            onChange={handleChange}
+        />
+        <button className='todo-button'> add todo</button>
+    </form>
+  );
+}
+
+export default TodoForm
