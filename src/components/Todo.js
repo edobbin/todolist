6133cdd51d481417b3d27149d5bb2645a059ce9f
edobbin <ethandobbin37@gmail.com>
2023-10-25 14:13:20 -0400

    final

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,7 +8,7 @@ export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
     <div className='Todo'>
         <p onClick={()=> toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}` }>{task.task}</p>
         <div>
-        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faPenToSquare} onClick={()=> editTodo(task.id)}/>
         <FontAwesomeIcon icon={faTrash} onClick={()=> deleteTodo(task.id)} />
         </div>
     </div>
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4} from 'uuid'
 import { Todo } from './Todo'
+import { EditTodoForm } from './EditTodoForm'
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -12,17 +13,27 @@ export const TodoWrapper = () => {
     const addTodo = todo =>{
         setTodos([...todos, {id: uuidv4(), task: todo,completed: false, isEditing: false}])
     }
+    const editTodo = id =>{
+        setTodos(todos.map(todo=> todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+    }
     const toggleComplete=id=>{
         setTodos(todos.map(todo=>todo.id === id? {...todo, completed: !todo.completed} : todo))
     }
     const deleteTodo = id =>{
         setTodos(todos.filter(todo=> todo.id !== id))
     }
+    const editTask = (task, id) => {
+        setTodos(todos.map(todo=> todo.id === id ?{ ...todo, task, isEditing: !todo.isEditing} : todo))
+    }
   return (
     <div className='TodoWrapper'>
         <TodoForm addTodo={addTodo}/>
         {todos.map((todo, index)=> (
-        <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo = {deleteTodo}/>
+            todo.isEditing ?(
+                <EditTodoForm editTodo={editTask} task ={todo}/>
+            ):(
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo = {deleteTodo} editTodo={editTodo}/>
+            )
         ))}
     </div>
   )
